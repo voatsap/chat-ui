f00e9f38a119ff9d9f19eac50e0152e9f2e77788
Volodymyr Tsap <voa@shalb.com>
2023-11-22 13:50:15 +0200

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 # Create .env.local from environment variables
-echo "MONGODB_URL=${MONGODB_URL}" > .env.local
-echo "MODELS=${MODELS}" >> .env.local
-echo "OPENID_CONFIG=${OPENID_CONFIG}" >> .env.local
-echo "PUBLIC_APP_NAME=${PUBLIC_APP_NAME}" >> .env.local
-echo "PUBLIC_APP_ASSETS=${PUBLIC_APP_ASSETS}" >> .env.local
-echo "PUBLIC_APP_COLOR=${PUBLIC_APP_COLOR}" >> .env.local
-echo "PUBLIC_APP_DESCRIPTION=${PUBLIC_APP_DESCRIPTION}" >> .env.local
-echo "PUBLIC_APP_DATA_SHARING=${PUBLIC_APP_DATA_SHARING}" >> .env.local
-echo "PUBLIC_APP_DISCLAIMER=${PUBLIC_APP_DISCLAIMER}" >> .env.local
-echo "YDC_API_KEY=${YDC_API_KEY}" >> .env.local
-echo "SERPER_API_KEY=${SERPER_API_KEY}" >> .env.local
-echo "SERPAPI_KEY=${SERPAPI_KEY}" >> .env.local
-echo "chatPromptTemplate=${chatPromptTemplate}" >> .env.local
-echo "webSearchQueryPromptTemplate=${webSearchQueryPromptTemplate}" >> .env.local
-echo "USE_CLIENT_CERTIFICATE=${USE_CLIENT_CERTIFICATE}" >> .env.local
-echo "CERT_PATH=${CERT_PATH}" >> .env.local
-echo "KEY_PATH=${KEY_PATH}" >> .env.local
-echo "CLIENT_KEY_PASSWORD=${CLIENT_KEY_PASSWORD}" >> .env.local
-echo "CA_PATH=${CA_PATH}" >> .env.local
-echo "REJECT_UNAUTHORIZED=${REJECT_UNAUTHORIZED}" >> .env.local
+if [[ -z "${MONGODB_URL}" ]]; then
+  MongoDbPort="${MONGODB_PORT:-27017}"
+  MONGODB_URL=mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MongoDbPort}
+  if [[ -n "${MONGODB_URL_PARAMS}" ]]; then
+    MONGODB_URL="${MONGODB_URL}/${MONGODB_URL_PARAMS}"
+  fi
+fi
+echo "# Chat configuration" > .env.local # clear file
+
+# List all configuration variables (from .env) names and try to find environment with same name and set to .env.local
+cat /app/.env | grep -E "^[A-Z_]{2,}=" | awk -F = '{print $1}' | sort | uniq |  while read envName
+do
+  if [[ -n "${!envName}" ]]; then
+    echo "Set variable ${envName}"
+    echo "${envName}=${!envName}" >> .env.local
+  fi
+done
 
 # Build the application
 npm run build
 
 # Start the application with PM2
-pm2 start /app/build/index.js -i $CPU_CORES --no-daemon
+pm2 start /app/build/index.js -i "$CPU_CORES" --no-daemon
